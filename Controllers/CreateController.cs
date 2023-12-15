6a5bb7ad7f64c62063a12c52bd1522bc10838b2e
Atacan Aloğlu <alogluatacan@gmail.com>
2023-12-15 12:47:38 +0300

    crtctrl1

---
+++ Controllers/CreateController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinimalJwt.Models;
+using MinimalJwt.Services;
+
+namespace MinimalJwt.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CreateController : Controller
+    {
+        private readonly IMovieService _movieService;
+        public CreateController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+      
+
+
+        IResult Create(Movie movie, IMovieService service)
+        {
+            var result = service.Create(movie);
+            return Results.Ok(result);
+        }
+    
+
+    //public IActionResult Create([FromBody] Movie movie)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        var createdMovie = _movieService.Create(movie);
+
+    //        if (createdMovie != null)
+    //        {
+    //            // Assuming your IMovieService returns the created movie upon success
+    //            return Ok(createdMovie);
+    //        }
+    //        else
+    //        {
+    //            // You can customize the response for failure if needed.
+    //            return BadRequest("Failed to create movie");
+    //        }
+    //    }
+    }
+}
